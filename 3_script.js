0f4e510461b50ab2809efabd266565eb795b9fd2
fuy-hub <a0900319068@gmail.com>
2024-12-13 18:37:51 +0800

    Add files via upload

---
+++ 3_script.js
@@ -0,0 +1,11 @@
+document.getElementById('refreshStatus').addEventListener('click', function() {
+    alert('正在刷新列印機狀態...');
+});
+
+document.getElementById('startPrint').addEventListener('click', function() {
+    alert('已啟動列印作業！');
+});
+
+document.getElementById('stopPrint').addEventListener('click', function() {
+    alert('已停止列印作業！');
+});
