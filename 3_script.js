e48de995d172d26d8d9c2165c8cf46f1998a8a59
fuy-hub <a0900319068@gmail.com>
2024-12-14 18:59:54 +0800

    Add files via upload

--- 3_script.js
+++ 3_script.js
@@ -1,14 +1,20 @@
 // 為「刷新狀態」按鈕添加點擊事件處理器
-document.getElementById('refreshStatus').addEventListener('click', function() {
+//document.getElementById('refreshStatus').
+addEventListener('click', function() {
     alert('正在刷新列印機狀態...'); // 顯示提醒訊息，告知正在刷新列印機狀態
 });
 
 // 為「啟動列印」按鈕添加點擊事件處理器
-document.getElementById('startPrint').addEventListener('click', function() {
-    alert('已啟動列印作業！'); // 顯示提醒訊息，告知列印作業已啟動
+//document.getElementById('startPrint').
+addEventListener('click', function() {
+    alert('已在施壓線頭作業！'); // 顯示提醒訊息，告知列印作業已啟動
 });
 
 // 為「停止列印」按鈕添加點擊事件處理器
-document.getElementById('stopPrint').addEventListener('click', function() {
-    alert('已停止列印作業！'); // 顯示提醒訊息，告知列印作業已停止
+//document.getElementById('stopPrint').
+addEventListener('click', function() {
+    alert('已在施按進料作業！'); // 顯示提醒訊息，告知列印作業已停止
+});
+ddEventListener('click', function() {
+    alert('已在施按確定作業！'); 
 });
